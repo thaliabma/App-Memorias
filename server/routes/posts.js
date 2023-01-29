d74cf80067097254d23f52ebb41c9fdbbd91fc10
thaliabma <tbma@ic.ufal.br>
2023-01-29 16:09:36 -0300

    Implementando update do post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {getPost,createPost} from '../controllers/posts.js'
+import {getPost,createPost,updatePost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPost)
 router.post('/', createPost)
-
+router.patch('/:id', updatePost)
 export default router
